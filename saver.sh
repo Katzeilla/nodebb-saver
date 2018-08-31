aa3b696ebe84538430ad1989ca04e9f9e63d6063
Katzeilla <hello@katzeilla.org>
2018-08-30 21:28:18 -0400

    Check $Cookies and $URL

--- saver.sh
+++ saver.sh
@@ -1,2 +1,16 @@
 #! /bin/bash
 
+if [[ $URL == '' ]];
+then
+	echo 'You have to set $URL first.'
+	echo "\$ export URL='https://example.com' "
+	exit 1
+fi
+
+if [[ $Cookies == '' ]];
+then
+	echo 'You have to set $Cookies first.'
+	echo "\$ export Cookies='express.sid=\"s:YOURCOOKIES\"' "
+	exit 2 
+fi
+
