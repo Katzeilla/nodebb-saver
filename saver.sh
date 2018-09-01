2dca537568256ae7773f30fc4c54eaf833bd0158
Katzeilla <hello@katzeilla.org>
2018-08-31 21:55:23 -0400

    save_topic

--- saver.sh
+++ saver.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+Topic_ID=40
+retry_Count=0
+
+
 function check_env {
 if [[ $Saver_URL == '' ]]; then
 	echo 'Error: You have to set $Saver_URL first.'
@@ -26,14 +30,20 @@ fi
 
 }
 
-# get_topic <topic_ID>
+# get_topic <topic_ID> [<page_ID>]
 # set $result to json response
 
 function get_topic {
-	result=$(curl -b $Saver_Cookies $Saver_URL/api/topic/$1)
+	if [[ $2 == ''  ]]; then
+		result=$(curl -b $Saver_Cookies $Saver_URL/api/topic/$1)
+	else
+		result=$(curl -b $Saver_Cookies $Saver_URL/api/topic/$1?page=$2)
+		
+	fi
+
 }
 
-# get_pageCount 
+
 # return page count for $result
 
 function get_pageCount {
@@ -49,20 +59,43 @@ function topicFound {
 }
 
 
+# save_topic <Topic_ID>
+# Save Json to disk
+
+function save_topic {
+	if [ ! -d $dir ]; then
+		mkdir ./$dir  	
+	fi
+
+	echo $result > ./$dir/Topic_$Topic_ID.json
+
+	if [[ $(get_pageCount) > 1  ]]; then 
+	echo "LT 1"
+	fi	
+}
+
+
 function main {
 	check_env
 #	get_topic 41	
 #	get_pageCount
 	
-	Topic_ID=1
 	while true
 	do
 		echo "Get topic $Topic_ID"
 		get_topic $Topic_ID
-			if [ topicFound ]; then
-			echo $result | jq
-			fi
-		Topic_ID=$(( $Topic_ID + 1 ))
+
+		if [ topicFound ]; then
+			save_topic	
+			Topic_ID=$(( $Topic_ID + 1 ))
+		else
+		    if [[ $retry_Count -le 10  ]]; then
+			    Topic_ID=$(( $Topic_ID + 1 ))
+		    else
+			    echo "Done!"
+		    fi
+
+		fi
 	done
 
 }
